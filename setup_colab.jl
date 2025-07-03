36d5eadd6f40a1f49cbaac3a2d7d3f607cf0a5aa
larsenno <lars.enhonsel@gmail.com>
2025-07-03 22:47:04 +0200

    Fix

--- setup_colab.jl
+++ setup_colab.jl
@@ -4,6 +4,6 @@ Pkg.instantiate()
 
 cd("../.")
 run(`git clone https://github.com/GlobalClimateForum/DIVACoast.jl`)
-pwd("./DIVACoast.jl")
+cd("./DIVACoast.jl")
 include("./src/DIVACoast.jl")
 using .DIVACoast
