99aa9a4bb55dfd9d59f3e36b8865947c50f42f77
larsenno <lars.enhonsel@gmail.com>
2025-07-03 22:52:40 +0200

    Added Logs

--- setup_colab.jl
+++ setup_colab.jl
@@ -1,7 +1,9 @@
+@info "Instantiating Dependencies"
 using Pkg
 Pkg.activate(".")
 Pkg.instantiate()
 
+@info "Getting DIVACoast.jl"
 cd("../.")
 run(`git clone https://github.com/GlobalClimateForum/DIVACoast.jl`)
 cd("./DIVACoast.jl")
