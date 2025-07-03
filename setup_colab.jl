57dc801934055b6b29a4464c06f96e13a4c9f17c
larsenno <lars.enhonsel@gmail.com>
2025-07-03 22:56:13 +0200

    Debug

--- setup_colab.jl
+++ setup_colab.jl
@@ -7,6 +7,5 @@ Pkg.instantiate()
 cd("../.")
 run(`git clone https://github.com/GlobalClimateForum/DIVACoast.jl`)
 cd("./DIVACoast.jl")
-println("Current directory: ", pwd())
-# include("./src/DIVACoast.jl")
-# using .DIVACoast
+include("/src/DIVACoast.jl")
+using .DIVACoast
